943d9356291f6a28aeefac610169be0db0bb2e37
Gabriel Kleber <gabrielklebern@gmail.com>
2025-07-06 15:40:22 -0300

    Básico

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./home.module.css";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className={styles.titulo}>oi</h1>
+      <h2>Olá, mundo!</h2>
+    </>
+  );
+};
+
+export default Home;
